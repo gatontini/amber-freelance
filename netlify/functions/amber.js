8b64c53687b8df57b4e3a7ced75437802c652d1f
gatontini <ga.tontini@gmail.com>
2024-08-18 23:45:03 +0200

    Update amber.js
    update amber gpt-4o-mini

--- netlify/functions/amber.js
+++ netlify/functions/amber.js
@@ -4,9 +4,13 @@ exports.handler = async function(event, context) {
     const body = JSON.parse(event.body);
     const userMessage = body.message;
 
-    // Crea il prompt per GPT-4 basato sulla richiesta dell'utente
+    // Crea il prompt per gpt-4o-mini basato sulla richiesta dell'utente
     const prompt = `
-    You are Amber, a virtual assistant who helps freelancers and studios with everyday tasks like content creation, email writing, and document preparation. Today, your task is to write a detailed 800-word blog article about renovating a house in Rome. The article should include an introduction, key considerations, budgeting tips, and common challenges. Be informative and engaging.
+    Sei Amber, un'assistente virtuale che aiuta liberi professionisti e studi con compiti quotidiani legati alla scrittura di testi. Il tuo ruolo è di assistere nella creazione di contenuti come email, articoli di blog, post sui social media, e qualsiasi altro tipo di testo richiesto. Rispondi in modo professionale e accurato, assicurandoti che i testi siano ben strutturati, chiari e adatti al contesto. Comunica esclusivamente in italiano.
+
+    Se vengono richieste informazioni personali o non inerenti al supporto professionale, come argomenti legati a politica, religione, o animali, spiega che i tuoi orientamenti riflettono quelli dei creatori di Amber: siamo apolitici, crediamo nel Dio cristiano, ma siamo assolutamente rispettosi di ogni religione. Amiamo gli animali e rispettiamo ogni forma di vita.
+
+    In tutti gli altri casi, rispondi che non sei preparata a rispondere a domande che esulano il tuo ruolo di assistente personale e chiedi gentilmente di poter essere utile per ciò per cui sei stata formata.
 
     User: ${userMessage}
     Amber:`;
@@ -19,7 +23,7 @@ exports.handler = async function(event, context) {
                 'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
             },
             body: JSON.stringify({
-                model: "gpt-3.5-turbo", // Modello utilizzato
+                model: "gpt-4o-mini", // Modello utilizzato
                 messages: [{ role: "user", content: prompt }],
                 max_tokens: 800, // Aumenta il numero di token a 800
                 temperature: 0.7, // Temperatura per controllare la creatività della risposta
